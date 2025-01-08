1140b84e37a8dd51eedd9a0e80f504d22abfb895
Baribor <justluvia@gmail.com>
2025-01-08 16:17:51 +0100

    fix file path

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
-import Home from "./components/bodyComponents/temp_home/home";
+import Home from "./components/bodyComponents/home/Home";
 import Inventory from "./components/bodyComponents/inventory/Inventory";
 import Customer from "./components/bodyComponents/customer/Customer";
 import Revenue from "./components/bodyComponents/revenue/Revenue";
 import Growth from "./components/bodyComponents/growth/Growth";
 import Report from "./components/bodyComponents/report/Report";
-import Setting from "./components/bodyComponents/temp_settings/Setting";
+import Setting from "./components/bodyComponents/settings/Setting";
 import Order from "./components/bodyComponents/order/Order";
 // import OrderModal from "./components/bodyComponents/order/OrderModal";
 
